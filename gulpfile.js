38cb1a087530aa04c42b925aa634648e3bf2a592
Rebecca Butterfield <rebecca.butterfield@nextcentury.com>
2016-11-21 17:04:30 -0500

    [A-3881] Listening for remote debuggers, making server node file patterns handle nested dirs, starting simple string util

--- gulpfile.js
+++ gulpfile.js
@@ -38,6 +38,12 @@ colors.enabled = true;
  * @returns {*}
  */
 function nodemon(nodemonConfig) {
+	let config = require('./src/server/config');
+	let nodeArgs = ['--debug=' + config.devPorts.debug];
+	if(plugins.util.env.debugBrk) {
+		nodeArgs.push('--debug-brk');
+	}
+	nodemonConfig.nodeArgs = nodeArgs;
 	let stream = plugins.nodemon(nodemonConfig);
 	stream
 		.on('restart', () => {
