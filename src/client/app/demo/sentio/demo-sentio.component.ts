a6f6b8f81c048e356a262252ece2d57c593ed049
reblace <reblace@gmail.com>
2016-11-27 20:25:55 -0500

    These are fixes to get this stuff working with the latest sentio that supports d3v4

--- src/client/app/demo/sentio/demo-sentio.component.ts
+++ src/client/app/demo/sentio/demo-sentio.component.ts
@@ -13,7 +13,7 @@ export class DemoSentioComponent {
 		resize: true,
 		model: [],
 		configure: (chart: any) => {
-			chart.color(d3.scale.ordinal().range(['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c']));
+			chart.color(d3.scaleOrdinal().range(['#a6cee3', '#1f78b4', '#b2df8a', '#33a02c']));
 			chart.label(function(d: any) { return d.key + ' (' + d.value + ')'; });
 		},
 		update: () => {
