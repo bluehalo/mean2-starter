09733c3ce2c01164b5644371957900ec5995241c
Matt <mgaulin@asymmetrik.com>
2017-02-21 16:38:05 -0500

    Allow for kafka key in publishProvider

--- src/server/app/util/providers/kafka.server.provider.js
+++ src/server/app/util/providers/kafka.server.provider.js
@@ -4,6 +4,10 @@ let path = require('path'),
 	deps = require(path.resolve('./src/server/dependencies.js')),
 	kafkaProducer = deps.kafkaProducer;
 
-exports.publish = function(destination, message, retry) {
+exports.publish = function(destination, message, retry, key) {
+	if (key) {
+		return kafkaProducer.sendMessageForTopicWithKey(destination, JSON.stringify(message), key, false);
+	}
+
 	return kafkaProducer.sendMessageForTopic(destination, JSON.stringify(message), retry);
 };
