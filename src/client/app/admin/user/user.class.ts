6a787a27627d6932744ae806157a4d0cdd4991cc
sieabah <christopher.sidell@gmail.com>
2017-04-06 22:26:14 -0400

    Remove visibility definitions

--- src/client/app/admin/user/user.class.ts
+++ src/client/app/admin/user/user.class.ts
@@ -1,9 +1,9 @@
 import { Credentials } from '../authentication/credentials.class';
 
 export class User {
-	public userModel: any = {}; // raw User model object,
-	public credentials: Credentials = new Credentials();
-	public eua?: any;
+	userModel: any = {}; // raw User model object,
+	credentials: Credentials = new Credentials();
+	eua?: any;
 
 	// General auth methods
 	public isAuthenticated(): boolean {
