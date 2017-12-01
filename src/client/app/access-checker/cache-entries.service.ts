15cb24a738379054bde2a169da6071de3e676ada
Ying Mao <ymao@asymmetrik.com>
2017-12-01 13:43:39 -0500

    access checker module

--- src/client/app/access-checker/cache-entries.service.ts
+++ src/client/app/access-checker/cache-entries.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
-import { Response } from '@angular/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 import { Observable } from 'rxjs/Observable';
 
 import { AsyHttp, HttpOptions } from '../shared/asy-http.service';
-import { PagingOptions } from '../shared/pager.component';
+import { PagingOptions, IPagingResults, NULL_PAGING_RESULTS } from '../shared/pager.component';
+import { AlertService } from '../shared/alert.service';
 
 export class CacheEntry {
 	date: Date;
@@ -20,24 +21,52 @@ export class CacheEntry {
 
 @Injectable()
 export class CacheEntriesService {
+
 	constructor(
-		private asyHttp: AsyHttp
+		private asyHttp: AsyHttp,
+		private alertService: AlertService
 	) {}
 
-	public match(query: any, search: string, paging: PagingOptions): Observable<Response> {
-		return this.asyHttp.post(new HttpOptions('access-checker/entries/match?' + this.asyHttp.urlEncode(paging.toObj()), () => {}, { s: search, q: query }));
+	match(query: any, search: string, paging: PagingOptions): Observable<IPagingResults> {
+		return this.asyHttp.post(new HttpOptions('access-checker/entries/match?' + this.asyHttp.urlEncode(paging.toObj()), () => {}, { s: search, q: query }))
+			.map((result: IPagingResults) => {
+				if (null != result && Array.isArray(result.elements)) {
+					result.elements = result.elements.map((element: any) => ( { entry: new CacheEntry(element.key, element.value, element.ts), isRefreshing: false } ));
+				}
+
+				return result;
+			})
+			.catch((error: HttpErrorResponse) => {
+				this.alertService.addAlert(error.error.message);
+				return Observable.of(NULL_PAGING_RESULTS);
+			});
 	}
 
-	public remove(key: string): Observable<Response> {
-		return this.asyHttp.delete(new HttpOptions('access-checker/entry/' + encodeURIComponent(key), () => {}, { key: key }));
+	remove(key: string): Observable<boolean> {
+		return this.asyHttp.delete(new HttpOptions('access-checker/entry/' + encodeURIComponent(key), () => {}, { key: key }))
+			.map(() => {
+				this.alertService.addAlert(`Deleted cache entry: ${key}`, 'success');
+				return true;
+			})
+			.catch((error: HttpErrorResponse) => {
+				this.alertService.addAlert(error.error.message);
+				return Observable.of(false);
+			});
 	}
 
-	public refresh(key: string): Observable<Response> {
-		return this.asyHttp.post(new HttpOptions('access-checker/entry/' + encodeURIComponent(key), () => {}, { key: key }));
+	refresh(key: string): Observable<boolean> {
+		return this.asyHttp.post(new HttpOptions('access-checker/entry/' + encodeURIComponent(key), () => {}, { key: key }))
+			.map(() => {
+				this.alertService.addAlert(`Refreshed cache entry: ${key}`, 'success');
+				return true;
+			})
+			.catch((error: HttpErrorResponse) => {
+				this.alertService.addAlert(error.error.message);
+				return Observable.of(false);
+			});
 	}
 
-	public refreshCurrentUser(): Observable<Response> {
+	refreshCurrentUser(): Observable<any> {
 		return this.asyHttp.post(new HttpOptions('access-checker/user', () => {}));
 	}
-
 }
